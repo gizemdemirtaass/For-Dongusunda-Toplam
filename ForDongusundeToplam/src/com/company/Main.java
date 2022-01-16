f0012bcfdf22565d4a44e3152a057992995ff757
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-16 16:12:12 +0300

    Add files via upload

---
+++ ForDongusundeToplam/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int n=100, toplam=0;
+    for(int i=0; i<=n; i++){
+        toplam +=i;
+    }
+    System.out.println("Toplam : "+toplam);
+    }
+}
